75bd2288ba900ab9c3d0149064e3b9a2c72d66fc
Merwan Achibet <achibet.merwan@gmail.com>
2023-05-24 12:37:33 +0200

    Game Boy: clean up global variables

--- src/gameboy.rs
+++ src/gameboy.rs
@@ -1,8 +1,10 @@
-use mizu_core::{GameBoy, JoypadButton};
-
 use crate::ruby::{array::RubyArray, numeric::RubyInt, RubyString, Value, NIL};
+use mizu_core::{GameBoy, JoypadButton};
+use std::cell::RefCell;
 
-static mut GAMEBOY: Option<GameBoy> = None;
+thread_local!(
+    static GAMEBOY: RefCell<Option<GameBoy>> = RefCell::new(Option::None);
+);
 
 pub fn load_rom(_rb_module: Value, rb_path: Value) -> Value {
     let rb_path: RubyString = rb_path.into();
@@ -10,7 +12,7 @@ pub fn load_rom(_rb_module: Value, rb_path: Value) -> Value {
 
     let gameboy = GameBoy::builder(&path).build().unwrap();
 
-    unsafe { GAMEBOY = Some(gameboy) };
+    GAMEBOY.with(|gb| gb.borrow_mut().replace(gameboy));
 
     return NIL;
 }
@@ -31,17 +33,23 @@ fn get_joypad_button(rb_button_name: Value) -> JoypadButton {
     }
 }
 pub fn press_button(_rb_module: Value, rb_button_name: Value) -> Value {
-    if let Some(gb) = unsafe { &mut GAMEBOY } {
-        gb.press_joypad(get_joypad_button(rb_button_name));
-    }
+    GAMEBOY.with(|gb| {
+        gb.borrow_mut().as_mut().map(|gb| {
+            gb.press_joypad(get_joypad_button(rb_button_name));
+            gb
+        });
+    });
 
     return NIL;
 }
 
 pub fn release_button(_rb_module: Value, rb_button_name: Value) -> Value {
-    if let Some(gb) = unsafe { &mut GAMEBOY } {
-        gb.release_joypad(get_joypad_button(rb_button_name));
-    }
+    GAMEBOY.with(|gb| {
+        gb.borrow_mut().as_mut().map(|gb| {
+            gb.release_joypad(get_joypad_button(rb_button_name));
+            gb
+        });
+    });
 
     return NIL;
 }
@@ -51,21 +59,25 @@ pub fn run_frame(_rb_module: Value, rb_frame_count: Value) -> Value {
 
     let rb_screen_buffer = RubyArray::new();
 
-    if let Some(gb) = unsafe { &mut GAMEBOY } {
-        // Run
+    GAMEBOY.with(|gb| {
+        gb.borrow_mut().as_mut().map(|gb| {
+            // Run
 
-        for _ in 0..frame_count {
-            gb.clock_for_frame();
-        }
+            for _ in 0..frame_count {
+                gb.clock_for_frame();
+            }
 
-        // Retrieve screen data
+            // Retrieve screen data
 
-        let screen = gb.screen_buffer();
+            let screen = gb.screen_buffer();
 
-        for pixel in screen {
-            rb_screen_buffer.push(RubyInt::new(*pixel as i64));
-        }
-    }
+            for pixel in screen {
+                rb_screen_buffer.push(RubyInt::new(*pixel as i64));
+            }
+
+            gb
+        });
+    });
 
     return rb_screen_buffer.into();
 }
